3e863232382a68562afd8e905e0912ed732253d8
boudzmarcellino <boudzmarcellino@gmail.com>
2023-12-21 04:24:46 +0100

    Initial commit

---
+++ app/include/main.h
@@ -0,0 +1,20 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f401xe.h"
+
+
+void Error_Handler(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
