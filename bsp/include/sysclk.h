fbfb9b3dc6f098ce0e9235a9c76c5b4c559bfc4e
boudzmarcellino <boudzmarcellino@gmail.com>
2024-01-15 23:26:11 +0100

    Enable clock
    USART2 clock on the APB2ENR for alternate function AF07 with GPIOA
    GPIOA clock from AHB1
    
    	SEE SYSCLK module on init function

--- bsp/include/sysclk.h
+++ bsp/include/sysclk.h
@@ -1,3 +1,8 @@
+#define RCC_MASK  0x00000000
 #define GPIOAEN  (1U<<0)
+#define USART2EN  (1U<<17)
+#define USART1EN (1U<<4)
 
-void SYSCLK_gpio();
+void SYSCLK_init();
+void SYSCLK_enable();
+void SYSCLK_config();
--- bsp/src/sysclk.c
+++ bsp/src/sysclk.c
@@ -1,11 +1,27 @@
 
 #include "stm32f401xe.h"
 #include "sysclk.h"
-void SYSCLK_gpioA(){
-
-    //TODO : Enable the AHB 
-    RCC->AHB1ENR |= GPIOAEN; 
-    //TODO : Enable AHB1
-    //TODO : Enable ClkGPIO
+void SYSCLK_init(){
+//    (*RCC) &=  RCC_MASK; 
+}
+void SYSCLK_config(){
     
+};
+
+void SYSCLK_enable(){
+/** POSSIBILITIES TO CONFIGURE CLOCK ON NORMAL POWER OR LOW POEWER  **/
+    
+//PERIPHERAL CLOCKS ON THE AHB BUSE
+    //TODO : Enable GPIO clock for each port in use
+    RCC->AHB1ENR |= GPIOAEN; 
+
+//PERIPHERAL CLOCKS ON THE APB1 BUSE
+    //TODO : Enable USART2 clock
+    RCC->APB1ENR |= USART1EN;
+ 
+//PERIPHERAL CLOCKS ON THE APB2 BUSE
+    //TODO : Enable USART2 clock
+    RCC->APB2ENR |= USART2EN;
+
+
 }
